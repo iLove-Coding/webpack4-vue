8b2328effda1d29919c7ff2c8f73ff3622e7b2a6
iLove-Coding <33741919+iLove-Coding@users.noreply.github.com>
2018-10-08 19:50:05 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Vue from "vue";
 import router from "./router"; // 路由
 import store from "./store"; // 状态管理
 
-import App from "App"; // 导入App组件（为什么这样导入？请看第四段中的第7）
+import App from "App"; // 导入App组件
 
 new Vue({
     router,
